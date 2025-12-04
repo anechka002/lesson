ffddd2d306d3fb047c56c8839fa1e3b0052e1a23
Anna <blinova.anneta@gmail.com>
2025-12-04 14:18:55 +0100

    add env for api-key

--- src/dal/api.ts
+++ src/dal/api.ts
@@ -1,19 +1,24 @@
 import type {GlobalTaskListItemJsonApiData, TaskDetailsData} from "../types/types.ts";
 
+const prepareHeaders = () => {
+  const apiKey = import.meta.env.VITE_API_KEY
+  if (!apiKey) return undefined
+
+  return {
+    'api-key': apiKey
+  }
+}
+
 export const getTasks = () => {
   const promise: Promise<{data: GlobalTaskListItemJsonApiData[]}> = fetch('https://trelly.it-incubator.app/api/1.0/boards/tasks', {
-    headers: {
-      'api-key': 'e89a9a5a-8ec8-4868-866c-0e822747b9ad'
-    }
+    headers: prepareHeaders()
   }).then(res => res.json())
   return promise
 }
 
 export const getTask = (boardId: string, id: string) => {
   const promise: Promise<{data: TaskDetailsData}> = fetch(`https://trelly.it-incubator.app/api/1.0/boards/${boardId}/tasks/${id}`, {
-    headers: {
-      'api-key': 'e89a9a5a-8ec8-4868-866c-0e822747b9ad'
-    }
+    headers: prepareHeaders()
   }).then(res => res.json())
   return promise
 }
